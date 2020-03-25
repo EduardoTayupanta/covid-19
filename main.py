71cad022faeba64a5b258ff7103838624204bcd8
etayupanta <wetayupanta@gmail.com>
2020-03-25 01:40:06 -0500

    Prueba inicial

---
+++ main.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created by etayupanta at 3/25/2020 - 01:23
+PyCharm - covid-19
+__author__ = 'Eduardo Tayupanta'
+__email__ = 'etayupanta@yotec.tech'
+"""
+
+
+# Import Libraries:
+
+
+def main():
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
